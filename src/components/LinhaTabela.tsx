f1d4646d418909cabf2374814090717cdd176a9d
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-11-21 22:49:48 -0300

    Adiciona lógica para linhas cinzas no quadro de horários

--- src/components/LinhaTabela.tsx
+++ src/components/LinhaTabela.tsx
@@ -1,26 +1,50 @@
 import "./LinhaTabela.css";
 
-type Props ={
-    cel1: string;
-    cel2: string;
-    cel3: string;
-    cel4: string;
-    cel5: string;
-    cel6: string;
-    cel7: string;
-}
+type Props = {
+  cel1: string;
+  cel2: string;
+  cel3: string;
+  cel4: string;
+  cel5: string;
+  cel6: string;
+  cel7: string;
+  cinza: boolean;
+};
 
-function LinhaTabela({cel1, cel2, cel3, cel4, cel5, cel6, cel7}:Props) {
+function LinhaTabela({
+  cel1,
+  cel2,
+  cel3,
+  cel4,
+  cel5,
+  cel6,
+  cel7,
+  cinza,
+}: Props) {
   return (
-    <tr className="linha">
-      <td>{cel1}</td>
-      <td>{cel2}</td>
-      <td>{cel3}</td>
-      <td>{cel4}</td>
-      <td>{cel5}</td>
-      <td>{cel6}</td>
-      <td>{cel7}</td>
-    </tr>
+    <>
+      {cinza ? (
+        <tr className="linha cinza">
+          <td>{cel1}</td>
+          <td>{cel2}</td>
+          <td>{cel3}</td>
+          <td>{cel4}</td>
+          <td>{cel5}</td>
+          <td>{cel6}</td>
+          <td>{cel7}</td>
+        </tr>
+      ) : (
+        <tr className="linha">
+          <td>{cel1}</td>
+          <td>{cel2}</td>
+          <td>{cel3}</td>
+          <td>{cel4}</td>
+          <td>{cel5}</td>
+          <td>{cel6}</td>
+          <td>{cel7}</td>
+        </tr>
+      )}
+    </>
   );
 }
 
--- src/components/QuadroHorarios.tsx
+++ src/components/QuadroHorarios.tsx
@@ -7,23 +7,23 @@ function QuadroHorarios() {
 
       <section>
         <table>
-          <LinhaTabela cel1="" cel2="Segunda" cel3="Terça" cel4="Quarta" cel5="Quinta" cel6="Sexta" cel7="Sábado"/>
-          <LinhaTabela cel1="07:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="08:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="09:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="10:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="11:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="12:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="13:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="14:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="15:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="16:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="17:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="18:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
-          <LinhaTabela cel1="19:00" cel2="DCC207-A" cel3="AAA000-X" cel4="BBB000-X" cel5="AAA000-X" cel6="" cel7="" />
-          <LinhaTabela cel1="20:00" cel2="DCC207-A" cel3="AAA000-X" cel4="BBB000-X" cel5="AAA000-X" cel6="" cel7="" />
-          <LinhaTabela cel1="21:00" cel2="BBB000-X" cel3="CCC000-X" cel4="" cel5="DCC207-A" cel6="AAA000-X" cel7="" />
-          <LinhaTabela cel1="22:00" cel2="BBB000-X" cel3="CCC000-X" cel4="" cel5="DCC207-A" cel6="AAA000-X" cel7="" />
+          <LinhaTabela cinza={false} cel1="" cel2="Segunda" cel3="Terça" cel4="Quarta" cel5="Quinta" cel6="Sexta" cel7="Sábado"/>
+          <LinhaTabela cinza={true} cel1="07:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={false} cel1="08:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={true} cel1="09:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={false} cel1="10:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={true} cel1="11:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={false} cel1="12:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={true} cel1="13:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={false} cel1="14:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={true} cel1="15:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={false} cel1="16:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={true} cel1="17:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={false} cel1="18:00" cel2="" cel3="" cel4="" cel5="" cel6="" cel7="" />
+          <LinhaTabela cinza={true} cel1="19:00" cel2="DCC207-A" cel3="AAA000-X" cel4="BBB000-X" cel5="AAA000-X" cel6="" cel7="" />
+          <LinhaTabela cinza={false} cel1="20:00" cel2="DCC207-A" cel3="AAA000-X" cel4="BBB000-X" cel5="AAA000-X" cel6="" cel7="" />
+          <LinhaTabela cinza={true} cel1="21:00" cel2="BBB000-X" cel3="CCC000-X" cel4="" cel5="DCC207-A" cel6="AAA000-X" cel7="" />
+          <LinhaTabela cinza={false} cel1="22:00" cel2="BBB000-X" cel3="CCC000-X" cel4="" cel5="DCC207-A" cel6="AAA000-X" cel7="" />
         </table>
         <div></div> {/* turmas com horarios livres */}
       </section>
