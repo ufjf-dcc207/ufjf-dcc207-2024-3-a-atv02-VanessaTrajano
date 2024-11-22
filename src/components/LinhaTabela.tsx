e918d4d580cb1e3f8d73967a7860c6e3c14ae065
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-11-21 22:16:19 -0300

    Cria componente LinhaTabela

---
+++ src/components/LinhaTabela.tsx
@@ -0,0 +1,25 @@
+type Props ={
+    cel1: string;
+    cel2: string;
+    cel3: string;
+    cel4: string;
+    cel5: string;
+    cel6: string;
+    cel7: string;
+}
+
+function LinhaTabela({cel1, cel2, cel3, cel4, cel5, cel6, cel7}:Props) {
+  return (
+    <tr>
+      <td>{cel1}</td>
+      <td>{cel2}</td>
+      <td>{cel3}</td>
+      <td>{cel4}</td>
+      <td>{cel5}</td>
+      <td>{cel6}</td>
+      <td>{cel7}</td>
+    </tr>
+  );
+}
+
+export default LinhaTabela;
--- src/components/QuadroHorarios.tsx
+++ src/components/QuadroHorarios.tsx
@@ -1,28 +1,14 @@
+import LinhaTabela from "./LinhaTabela";
+
 function QuadroHorarios() {
   return (
     <>
       <h2>Quadro de Horários</h2>
 
       <section>
-        <div>
-          <div></div> {/* dias da semana */}
-          <div></div> {/* 7h */}
-          <div></div> {/* 8h */}
-          <div></div> {/* 9h */}
-          <div></div> {/* 10h */}
-          <div></div> {/* 11h */}
-          <div></div> {/* 12h */}
-          <div></div> {/* 13h */}
-          <div></div> {/* 14h */}
-          <div></div> {/* 15h */}
-          <div></div> {/* 16h */}
-          <div></div> {/* 17h */}
-          <div></div> {/* 18h */}
-          <div></div> {/* 19h */}
-          <div></div> {/* 20h */}
-          <div></div> {/* 21h */}
-          <div></div> {/* 22h */}
-        </div>
+        <table>
+          <LinhaTabela cel1="" cel2="Segunda" cel3="Terça" cel4="Quarta" cel5="Quinta" cel6="Sexta" cel7="Sábado"/>
+        </table>
         <div></div> {/* turmas com horarios livres */}
       </section>
     </>
